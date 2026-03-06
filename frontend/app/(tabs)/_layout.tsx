479d8d81b2144eff8935efc386736d0f1ba3743a
Pagliaccio <payuk.pagliaccio@gmail.com>
2026-03-06 12:16:21 +0300

    return icon-symbol component

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
-import { Tabs } from 'expo-router'
 import React from 'react'
+import { Tabs } from 'expo-router'
+import { IconSymbol } from '@/components/ui/icon-symbol'
 
 export default function TabLayout() {
   return (
@@ -10,13 +11,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home'
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="shop"
         options={{
-          title: 'Shop'
+          title: 'Shop',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
