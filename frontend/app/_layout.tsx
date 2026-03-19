309e0562d1c3a2ee0b42995e04639adcaaf0779a
Pagliaccio <payuk.pagliaccio@gmail.com>
2026-03-20 00:06:14 +0300

    Revert "test changelogs"
    This reverts commit 562d42d1f867081ff411069f99835b8c029f1a2f.

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,30 +1,33 @@
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
 
 import { AuthProvider } from '@/context/auth-context'
 import { NotificationProvider } from '@/context/notification-context'
 import { useOTAUpdate } from '@/hooks/use-ota-update'
-import { ChangelogModal } from '@/components/modals/ChangelogModal'
+import { LoadingBanner } from '@/components/ui/LoadingBanner'
 
 export default function RootLayout() {
-  const { showChangelog, changelog, dismissChangelog } = useOTAUpdate()
+  const { isChecking, isUpdating } = useOTAUpdate()
+
+  if (isChecking) {
+    return <LoadingBanner message='Проверка обновлений...' />
+  }
+
+  if (isUpdating) {
+    return <LoadingBanner message='Загрузка обновления...' />
+  }
 
   return (
     <AuthProvider>
       <NotificationProvider>
         <Stack>
-          <Stack.Screen name="(tabs)"   options={{ headerShown: false }} />
-          <Stack.Screen name="login"    options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
           <Stack.Screen name="register" options={{ headerShown: false }} />
         </Stack>
-
+        
         <StatusBar style="auto" />
-
-        <ChangelogModal
-          visible={showChangelog}
-          changelog={changelog}
-          onDismiss={dismissChangelog}
-        />
       </NotificationProvider>
     </AuthProvider>
   )
